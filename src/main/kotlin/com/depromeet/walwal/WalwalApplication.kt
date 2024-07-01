2d3777210748778434214ee184733dcc6e6d050f
ybchar <uiurihappy@sju.ac.kr>
2024-07-01 15:46:34 +0900

    initial commit

---
+++ src/main/kotlin/com/depromeet/walwal/WalwalApplication.kt
@@ -0,0 +1,11 @@
+package com.depromeet.walwal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalwalApplication
+
+fun main(args: Array<String>) {
+	runApplication<WalwalApplication>(*args)
+}
---
+++ src/test/kotlin/com/depromeet/walwal/WalwalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.depromeet.walwal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WalwalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
