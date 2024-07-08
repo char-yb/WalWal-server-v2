fc266c75f0f19b77d7b3570267ec572a145ab5b3
ybchar <uiurihappy@sju.ac.kr>
2024-07-08 10:04:36 +0900

    chore: 필요 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,23 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Coroutines dependencies
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+
+	// DB
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
+
+	// redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	// fixture monkey dependencies
+	testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.0.20")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
