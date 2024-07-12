09a3349e2d8d4daee9488709360992bd5baae28a
ybchar <uiurihappy@sju.ac.kr>
2024-07-13 02:24:17 +0900

    chore: restClient Config

---
+++ src/main/kotlin/com/depromeet/walwal/global/config/restClient/RestClientConfig.kt
@@ -0,0 +1,21 @@
+package com.depromeet.walwal.global.config.restClient
+
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+import java.time.Duration
+
+@Configuration
+class RestClientConfig {
+    @Bean
+    fun restClient(): RestClient {
+        val restTemplate =
+            RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build()
+
+        return RestClient.create(restTemplate)
+    }
+}
