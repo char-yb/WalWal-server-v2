da34a0a5de4a953f1ad4147d40300afaa5e85530
ybchar <uiurihappy@sju.ac.kr>
2024-07-15 01:02:13 +0900

    chore: jacoco 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    jacoco
 }
 
 group = "com.depromeet"
@@ -66,6 +67,82 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+val jacocoExcludePatterns =
+    listOf(
+        "**/*Application*",
+        "**/*Config/*",
+        "**/resources/**",
+        "**/config/**",
+        "**/global/**",
+        "**/infra/**",
+        "**/test/**",
+        "**/*Constants/*",
+    )
+
+val jacocoDir = layout.buildDirectory.dir("reports/")
+
+tasks.jacocoTestReport<JacocoReport> {
+    dependsOn("test")
+    executionData(fileTree(jacocoDir.get()).include("jacoco/*.exec"))
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        csv.required.set(true)
+    }
+
+    finalizedBy("jacocoTestCoverageVerification")
+}
+
+tasks.jacocoTestCoverageVerification<JacocoCoverageVerification> {
+    violationRules {
+        rule {
+            // 'element'가 없으면 프로젝트의 전체 파일을 합친 값을 기준으로 한다.
+            limit {
+                // 'counter'를 지정하지 않으면 default는 'INSTRUCTION'
+                // 'value'를 지정하지 않으면 default는 'COVEREDRATIO'
+                minimum = "0.30".toBigDecimal()
+            }
+        }
+
+        rule {
+            // 룰을 간단히 켜고 끌 수 있다.
+            enabled = true
+
+            // 룰을 체크할 단위는 클래스 단위
+            element = "CLASS"
+
+//            // 브랜치 커버리지를 최소한 90% 만족시켜야 한다.
+//            limit {
+//                counter = "BRANCH"
+//                value = "COVEREDRATIO"
+//                minimum = "0.90".toBigDecimal()
+//            }
+//
+//            // 라인 커버리지를 최소한 80% 만족시켜야 한다.
+//            limit {
+//                counter = "LINE"
+//                value = "COVEREDRATIO"
+//                minimum = "0.30".toBigDecimal()
+//            }
+
+            // 빈 줄을 제외한 코드의 라인수를 최대 200라인으로 제한한다.
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "200".toBigDecimal()
+            }
+
+            // 커버리지 체크를 제외할 클래스들
+            excludes = jacocoExcludePatterns
+        }
+    }
 }
 
 // scripts 경로의 pre-commit hook 등록
@@ -84,4 +161,18 @@ tasks.register("addGitPreCommitHook", DefaultTask::class) {
 // compileKotlin가 addGitPreCommitHook에 의존하도록 설정
 tasks.named("compileKotlin") {
     dependsOn("addGitPreCommitHook")
+}
+
+val testCoverage by tasks.registering {
+    group = "verification"
+    description = "Runs the unit tests with coverage"
+
+    dependsOn(
+        ":test",
+        ":jacocoTestReport",
+        ":jacocoTestCoverageVerification",
+    )
+
+    tasks["jacocoTestReport"].mustRunAfter(tasks["test"])
+    tasks["jacocoTestCoverageVerification"].mustRunAfter(tasks["jacocoTestReport"])
 }
