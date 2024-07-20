fe12e6f3d19a40b1beb7ac80bb3a8065fc031517
ybchar <uiurihappy@sju.ac.kr>
2024-07-20 13:22:18 +0900

    chore: securityConstants 추가

---
+++ src/main/kotlin/com/depromeet/walwal/global/common/constants/SecurityConstants.kt
@@ -0,0 +1,11 @@
+package com.depromeet.walwal.global.common.constants
+
+object SecurityConstants {
+	const val TOKEN_ROLE_NAME: String = "role"
+	const val TOKEN_PREFIX: String = "Bearer "
+	const val APPLE_JWK_SET_URL: String = "https://appleid.apple.com/auth/keys"
+	const val APPLE_ISSUER: String = "https://appleid.apple.com"
+	const val APPLE_TOKEN_URL: String = "https://appleid.apple.com/auth/token"
+	const val ACCESS_TOKEN_COOKIE_NAME: String = "accessToken"
+	const val REFRESH_TOKEN_COOKIE_NAME: String = "refreshToken"
+}
