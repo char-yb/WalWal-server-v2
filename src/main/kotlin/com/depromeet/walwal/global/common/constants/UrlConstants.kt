549201cf9486b817c160a9e1b31818e6502808e1
ybchar <uiurihappy@sju.ac.kr>
2024-07-21 17:31:39 +0900

    chore: Swagger Config Auth 추가

---
+++ src/main/kotlin/com/depromeet/walwal/global/common/constants/UrlConstants.kt
@@ -0,0 +1,7 @@
+package com.depromeet.walwal.global.common.constants
+
+enum class UrlConstants(val value: String?) {
+	PROD_SERVER_URL("https://api.walwal.life"),
+	DEV_SERVER_URL("https://dev-api.walwal.life"),
+	LOCAL_SERVER_URL("http://localhost:8080"),
+}
--- src/main/kotlin/com/depromeet/walwal/global/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/depromeet/walwal/global/config/swagger/SwaggerConfig.kt
@@ -1,14 +1,21 @@
-package com.depromeet.walwal.global.config.swagger
+package com.depromeet.stonebed.global.config.swagger
 
+import com.depromeet.walwal.global.common.constants.UrlConstants
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.swagger.v3.core.jackson.ModelResolver
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig() {
 	@Value("\${api.version}")
 	private val apiVersion: String? = null
 
@@ -16,11 +23,35 @@ class SwaggerConfig {
 	@Bean
 	fun openAPI(): OpenAPI {
 		return OpenAPI()
+			.servers(swaggerServers())
+			.addSecurityItem(securityRequirement())
+			.components(authSetting())
 			.info(
 				Info()
-					.title("WalWal 프로젝트 API v2 문서화")
+					.title(SWAGGER_API_TITLE)
 					.version(apiVersion)
-					.description("WalWal 프로젝트의 Swagger UI 화면입니다."),
+					.description(SWAGGER_API_DESCRIPTION),
+			)
+	}
+
+	private fun swaggerServers(): List<Server> {
+		val localServer =
+			Server().url(UrlConstants.LOCAL_SERVER_URL.value).description(SWAGGER_API_DESCRIPTION)
+		val devServer =
+			Server().url(UrlConstants.DEV_SERVER_URL.value).description(SWAGGER_API_DESCRIPTION)
+		return listOf(localServer, devServer)
+	}
+
+	private fun authSetting(): Components {
+		return Components()
+			.addSecuritySchemes(
+				"accessToken",
+				SecurityScheme()
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JWT")
+					.`in`(SecurityScheme.In.HEADER)
+					.name("Authorization"),
 			)
 	}
 
@@ -28,12 +59,25 @@ class SwaggerConfig {
 	@Bean
 	fun groupedOpenApi(): GroupedOpenApi {
 		return GroupedOpenApi.builder()
-			.group("WalWal API v2")
+			.group("WalWal API")
 			.packagesToScan(PACKAGES_TO_SCAN)
 			.build()
 	}
 
+	private fun securityRequirement(): SecurityRequirement {
+		val securityRequirement = SecurityRequirement()
+		securityRequirement.addList("accessToken")
+		return securityRequirement
+	}
+
+	@Bean
+	fun modelResolver(objectMapper: ObjectMapper): ModelResolver {
+		return ModelResolver(objectMapper)
+	}
+
 	companion object {
-		private const val PACKAGES_TO_SCAN = "com.depromeet.walwal"
+		private const val PACKAGES_TO_SCAN = "com.depromeet.stonebed"
+		private const val SWAGGER_API_TITLE = "WalWal 프로젝트 API 문서"
+		private const val SWAGGER_API_DESCRIPTION = "WalWal 프로젝트 API 문서입니다."
 	}
 }
