3c0ef991ebeb12ced387f8cb20fc443eb641646f
ybchar <uiurihappy@sju.ac.kr>
2024-07-29 12:21:32 +0900

    chore: jwt 검증 SecurityConfig 구성 추가

--- src/main/kotlin/com/depromeet/walwal/global/config/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/depromeet/walwal/global/config/security/WebSecurityConfig.kt
@@ -1,6 +1,12 @@
 package com.depromeet.walwal.global.config.security
 
+import com.depromeet.walwal.domain.auth.application.JwtTokenService
 import com.depromeet.walwal.global.common.constants.SwaggerUrlConstants
+import com.depromeet.walwal.global.filter.JwtAuthenticationFilter
+import com.depromeet.walwal.global.util.CookieUtil
+import com.depromeet.walwal.infra.properties.SwaggerProperties
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
@@ -8,34 +14,65 @@ import org.springframework.http.HttpHeaders
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig {
+class WebSecurityConfig(
+	private val jwtTokenService: JwtTokenService,
+	private val cookieUtil: CookieUtil,
+	private val swaggerProperties: SwaggerProperties,
+) {
+
+	@Bean
+	fun inMemoryUserDetailsManager(): InMemoryUserDetailsManager {
+		val user =
+			User.withUsername(swaggerProperties.user)
+				.password(passwordEncoder().encode(swaggerProperties.password))
+				.roles("SWAGGER")
+				.build()
+		return InMemoryUserDetailsManager(user)
+	}
+
 	@Bean
 	@Throws(Exception::class)
 	fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+		apiSecurityFilterChain(http)
+
 		http.authorizeHttpRequests { authorize ->
 			authorize
 				.requestMatchers("/walwal-actuator/**")
-				.permitAll() // 액추에이터
+				.permitAll() // Actuator
+				.requestMatchers("/auth/**")
+				.permitAll() // Auth endpoints
 				.anyRequest()
 				.authenticated()
 		}
-
+			.exceptionHandling { exception: ExceptionHandlingConfigurer<HttpSecurity?> ->
+				exception.authenticationEntryPoint { _: HttpServletRequest?, response: HttpServletResponse, _: AuthenticationException? ->
+					response.status = 401
+				}
+			}
+		http.addFilterBefore(
+			jwtAuthenticationFilter(jwtTokenService, cookieUtil),
+			UsernamePasswordAuthenticationFilter::class.java,
+		)
 		return http.build()
 	}
 
 	@Bean
 	@Order(1)
-	@Throws(Exception::class)
 	fun swaggerFilterChain(http: HttpSecurity): SecurityFilterChain {
 		apiSecurityFilterChain(http)
 
@@ -56,6 +93,7 @@ class WebSecurityConfig {
 		http
 			.httpBasic { httpBasicConfigurer -> httpBasicConfigurer.disable() }
 			.formLogin { formLoginConfigurer -> formLoginConfigurer.disable() }
+			.csrf { csrfConfigurer -> csrfConfigurer.disable() }
 			.cors(withDefaults())
 			.sessionManagement { sessionManagementConfigurer ->
 				sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -76,5 +114,13 @@ class WebSecurityConfig {
 		val source = UrlBasedCorsConfigurationSource()
 		source.registerCorsConfiguration("/**", configuration)
 		return source
+	}
+
+	@Bean
+	fun jwtAuthenticationFilter(
+		jwtTokenService: JwtTokenService,
+		cookieUtil: CookieUtil,
+	): JwtAuthenticationFilter {
+		return JwtAuthenticationFilter(jwtTokenService, cookieUtil)
 	}
 }
